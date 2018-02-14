617bb2b9fde85899d0b95b4c1bb8db6a4c262e5b
gaetank <gaetan.kemayou@hotmail.fr>
2018-02-14 10:48:22 +0100

    connection database

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose =  require('./db/db');
+
 var app = express();
 
 app.use(logger('dev'));
